1272ea0a0d9e6d6c5c8ada0037e2b082ca6f029c
yusame <yu.starts.yu@gmail.com>
2023-05-27 23:13:12 +0900

    ABC303

--- AtCoder/C/ABC303-C.swift
+++ AtCoder/C/ABC303-C.swift
@@ -5,7 +5,8 @@
 //  Created by 小原宙 on 2023/05/27.
 //
 
-func main() {
+// 解けていない
+private func _main() {
     let numbers = readLine()!.split(separator: " ").map { Int(String($0))! }
     let moveCount = numbers[0]
     let itemCount = numbers[1]
