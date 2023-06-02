d0dc5dec9a5ca812447f94ca00e9776082c3acf8
yusame <yu.starts.yu@gmail.com>
2023-06-02 15:52:39 +0900

    private

--- AtCoder/sample.swift
+++ AtCoder/sample.swift
@@ -5,8 +5,8 @@
 //  Created by 小原宙 on 2023/05/01.
 //
 
-let number = Int(readLine()!)!
-let numbers = readLine()!.split(separator: " ").map { Int(String($0))! }
+//let number = Int(readLine()!)!
+//let numbers = readLine()!.split(separator: " ").map { Int(String($0))! }
 
 // 文字列置換
 // S.replacingOccurrences(of: "dream", with: ",")
